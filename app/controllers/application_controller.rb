e89d8bc02ed8de83bde6b52b28022e416c8c7982
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2022-10-11 07:59:20 +0000

    [Add]Gemの記載deviseのインストールmodelの作成controllerの作成Bootstrapのインストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def afte_sign_in_path_for(resource)
+    about_path
+  end
+
+  def afte_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.preoend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'juquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+  )
