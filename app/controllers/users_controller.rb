4f4d067edd8ac5fe077e31210fb3de8d92df7769
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2022-10-13 04:22:48 +0000

    [AddFix]部分テンプレートの作成SignInの不具合の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+  end
+
   def show
+    @user = User.find(params[:id])
+    @books = @user.books.all
+    @book = Book.new
   end
 
   def edit
